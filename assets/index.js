f7447f0442b1954a8f149ad4c89ab69ef85f4517
typex1 <8014314+typex1@users.noreply.github.com>
2022-03-03 14:43:55 +0100

    Lambda function with table query

---
+++ assets/index.js
@@ -0,0 +1,20 @@
+"use strict"
+const AWS = require("aws-sdk");
+exports.handler = async () => {
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region:"us-east-1" });
+    const params = {
+        TableName: "users",
+        Key: {
+            uid: "12345"
+    }
+};
+
+try{
+        const data = await documentClient.get(params).promise();
+        console.log('######### RESULT: ######################')
+        console.log(data);
+        console.log('########################################')
+    }catch(err){
+        console.log(err);
+    }
+};
